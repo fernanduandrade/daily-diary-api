dcd030a8f97b4531e937774bc617b19918f41456
fernanduandrade <onanduandrade@gmail.com>
2024-02-05 06:51:42 -0300

    fix: content concurrency

--- DailyDiary.Application/Likes/Favorite/FavoriteCommandHandler.cs
+++ DailyDiary.Application/Likes/Favorite/FavoriteCommandHandler.cs
@@ -9,22 +9,15 @@ namespace DailyDiary.Application.Likes.Favorite;
 public class CreateLikeCommandHandler : IRequestHandler<FavoriteCommand, ApiResponse<Unit>>
 {
     private readonly IUserLikeRepository _userLikeRepository;
-    private readonly IDiaryRepository _diaryRepository;
     private readonly IUnitOfWork _unitOfWork;
 
     public CreateLikeCommandHandler(IUnitOfWork unitOfWork, IUserLikeRepository userLikeRepository, IDiaryRepository diaryRepository)
     {
         _unitOfWork = unitOfWork;
         _userLikeRepository = userLikeRepository;
-        _diaryRepository = diaryRepository;
     }
     public async Task<ApiResponse<Unit>> Handle(FavoriteCommand request, CancellationToken cancellationToken)
     {
-        var diaryExist = _diaryRepository.GetById(request.DiaryId);
-        
-        if(diaryExist is null)
-            return ApiResponse<Unit>.Response(Unit.Value, "Error, Diary hasn't been liked", false);
-        
         var hasAlreadyLike = await _userLikeRepository.GetByUserAndDiaryId(request.UserId, request.DiaryId);
         if(hasAlreadyLike is not null)
             return ApiResponse<Unit>.Response(Unit.Value, "Already favorited", false);
--- DailyDiary.Application/Likes/UnFavorite/UnfavoriteCommandHandler.cs
+++ DailyDiary.Application/Likes/UnFavorite/UnfavoriteCommandHandler.cs
@@ -9,21 +9,15 @@ namespace DailyDiary.Application.Likes.UnFavorite;
 public class UnfavoriteCommandHandler : IRequestHandler<UnfavoriteCommand, ApiResponse<Unit>>
 {
     private readonly IUserLikeRepository _userLikeRepository;
-    private readonly IDiaryRepository _diaryRepository;
     private readonly IUnitOfWork _unitOfWork;
 
     public UnfavoriteCommandHandler(IUnitOfWork unitOfWork, IUserLikeRepository userLikeRepository, IDiaryRepository diaryRepository)
     {
         _unitOfWork = unitOfWork;
         _userLikeRepository = userLikeRepository;
-        _diaryRepository = diaryRepository;
     }
     public async Task<ApiResponse<Unit>> Handle(UnfavoriteCommand request, CancellationToken cancellationToken)
     {
-        var diaryExist = await _diaryRepository.GetById(request.DiaryId);
-        
-        if(diaryExist is null)
-            return ApiResponse<Unit>.Response(Unit.Value, "Error, can't unfavorite a diary that doesn't exist.", false); 
         var userLike = await _userLikeRepository.GetByUserAndDiaryId(request.UserId, request.DiaryId);
         if (userLike is null)
             return ApiResponse<Unit>.Response(Unit.Value, "Error, Diary hasn't been liked", false);
